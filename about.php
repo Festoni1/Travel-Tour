86f4edc64aa89a0d48e4458f49c774da2099a8a9
Festoni1 <73661187+Festoni1@users.noreply.github.com>
2025-07-10 13:27:14 +0200

    Travel Tour Page completed

--- about.php
+++ about.php
@@ -15,6 +15,23 @@
     
     <!-- custom css file link -->
     <link rel="stylesheet" href="css/style.css">
+    <script>
+  window.addEventListener('load', () => {
+    const slides = document.querySelectorAll('.reviews .swiper-slide');
+    let maxHeight = 0;
+
+    slides.forEach(slide => {
+      slide.style.height = 'auto'; // Reset first
+      if (slide.offsetHeight > maxHeight) {
+        maxHeight = slide.offsetHeight;
+      }
+    });
+
+    slides.forEach(slide => {
+      slide.style.height = maxHeight + 'px';
+    });
+  });
+</script>
 </head>
 <body>
     <!-- header section starts -->
@@ -30,9 +47,133 @@
         <div id="menu-btn" class="fas fa-bars"></div>
     </section>
     <!-- header section ends -->
-    <div class="heading" style="background:url(images/header-bg-1.jpg) no-repeat">
+    <div class="heading" style="background:url(images/header-bg.jpg) no-repeat">
         <h1>about us</h1>
     </div>
+
+
+    <!-- about section starts here  -->
+        <section class="about">
+            <div class="image">
+                <img src="images/about-img.jpg" alt="">
+            </div>
+
+            <div class="content">
+                <h3>why choose us</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla, id dicta. 
+                Doloribus in cum magni voluptate eius aspernatur possimus necessitatibus.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, officiis aliquam quo numquam voluptate ab?</p>
+
+            <div class="icons-container">
+                <div class="icons">
+                    <i class="fas fa-map"></i>
+                    <span>top destinations</span>
+                </div>
+                <div class="icons">
+                    <i class="fas fa-hand-holding-usd"></i>
+                    <span>affordable price</span>
+                </div>
+                <div class="icons">
+                    <i class="fas fa-headset"></i>
+                    <span>24/7 guide service</span>
+                </div>
+            </div>
+            </div>
+        </section>
+    <!-- about section ends here  -->
+
+
+    <!-- reviews section starts here  -->
+     <div class="reviews">
+     <h3>clients reviews</h3> 
+        <div class="swiper reviews-slider">
+
+            <div class="swiper-wrapper">
+
+                <div class="swiper-slide slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae in illum exercitationem possimus consequatur soluta atque placeat sunt autem sint consequuntur quisquam error earum, at temporibus a quo. Sunt, sequi.</p>
+                    <h3>john doe</h3>
+                    <span>traveler</span>
+                    <img src="images/pic-1.jpg" alt="">
+                </div>
+
+                <div class="swiper-slide slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit veniam id quibusdam, autem exercitationem molestias vero eaque atque tempore pariatur quaerat tempora magnam nam minus excepturi aperiam inventore? Optio aut architecto quisquam modi itaque minima omnis molestiae necessitatibus ab sit.</p>
+                    <h3>john doe</h3>
+                    <span>traveler</span>
+                    <img src="images/pic-2.jpg" alt="">
+                </div>
+
+                <div class="swiper-slide slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas necessitatibus consequatur odio nulla neque deleniti cumque quibusdam rerum iste atque aliquam, laboriosam eligendi quia dolores, hic accusantium officiis vero temporibus. Debitis asperiores modi placeat voluptatibus.</p>
+                    <h3>john doe</h3>
+                    <span>traveler</span>
+                    <img src="images/pic-3.jpg" alt="">
+                </div>
+
+                <div class="swiper-slide slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta quidem expedita maxime, quos fugiat ratione?</p>
+                    <h3>john doe</h3>
+                    <span>traveler</span>
+                    <img src="images/pic-4.png" alt="">
+                </div>
+
+                <div class="swiper-slide slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero architecto similique fuga, soluta perferendis, vel autem sit facilis quia veritatis eveniet, temporibus cumque animi sint!</p>
+                    <h3>john doe</h3>
+                    <span>traveler</span>
+                    <img src="images/pic-5.png" alt="">
+                </div>
+
+                <div class="swiper-slide slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos soluta rerum minus explicabo eligendi perspiciatis odio optio, adipisci mollitia laboriosam?</p>
+                    <h3>john doe</h3>
+                    <span>traveler</span>
+                    <img src="images/pic-6.png" alt="">
+                </div>
+            </div>
+            <div class="swiper-scrollbar"></div>
+        </div>
+     </div>
+    <!-- reviews section ends here  -->
 
 
     <!-- footer section starts -->
@@ -84,5 +225,6 @@
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <!-- custom js file link -->
     <script src="js/script.js"></script>
+    
 </body>
 </html>
--- home.php
+++ home.php
@@ -117,6 +117,65 @@
             </div>
         </section> 
      <!-- home about section ends -->
+
+
+
+     <!-- home packages sections starts  -->
+    
+     <section class="home-packages">
+        <h1 class="heading-title"> our packages </h1>
+        <div class="box-container">
+            <div class="box">
+                <div class="image">
+                    <img src="images/img-1.jpg" alt="">
+                </div>
+                <div class="content">
+                    <h3>adventure & tour</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, molestias!</p>
+                    <a href="book.php" class="btn">book now</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/img-2.jpg" alt="">
+                </div>
+                <div class="content">
+                    <h3>adventure & tour</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, molestias!</p>
+                    <a href="book.php" class="btn">book now</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/img-3.jpg" alt="">
+                </div>
+                <div class="content">
+                    <h3>adventure & tour</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, molestias!</p>
+                    <a href="book.php" class="btn">book now</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="load-more"><a href="package.php" class="btn">load more</a></div>
+     </section>
+     <!-- home packages section ends  -->
+
+
+     <!-- home offer section starts  -->
+    <section class="home-offer">
+        <div class="content">
+            <h3>up to 50% off!</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dicta saepe, quam minima numquam 
+                impedit consectetur quas voluptas dignissimos! Pariatur.</p>
+            <a href="book.php" class="btn">book now</a>
+        </div>
+    </section>
+
+     <!-- home offer section ends -->
+
     
      <!-- footer section starts -->
     <section class="footer">
